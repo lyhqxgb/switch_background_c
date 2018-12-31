4362eda91c05a9e1f44b14d84da9c92d14faa123
lyhqxgb <lyh52xx@126.com>
2019-01-01 00:22:11 +0800

    change ports into list && this is the first commit in new year

--- menu.c
+++ menu.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define SYSTEM_CLS system("cls")
 
 typedef struct port{
     char name[16];
@@ -17,12 +18,18 @@ typedef struct list_node{
 int login(void);
 void registry(void);
 void show_menu(void);
-void show_ports(struct port *p, int num);
-void set_ports(struct port **p, int *num);
-void set_port(struct port *p);
+void show_ports(node_t *head);
+void set_ports(node_t *head, int *num);
+void set_port(node_t *head, int place);
 void show_error(char *msg);
-int add_port(struct port *p, int *num);
+int add_port(node_t *head);
 node_t *list_init(void);
+void check_head(node_t *head);
+int free_list_node(node_t *head);//释放某一节点的空间
+typedef int (*free_fun_t)(node_t *);//给释放函数起别名
+int empty_list(node_t *head, free_fun_t);
+int destroy_list(node_t **head);
+int del_port(node_t *head);//询问用户要删除的端口，并返回删除结果
 
 int main(void)
 {
@@ -30,7 +37,7 @@ int main(void)
 
 
     while(1){
-        system("cls");//进入程序后，清屏
+        SYSTEM_CLS;//进入程序后，清屏
         printf("--------交换机后台-------\n");
         printf("1. 登陆\n");
         printf("2. 注册\n");
@@ -68,7 +75,7 @@ int login(void)
     int check = 0;//验证用户名和密码是否一致
     FILE * file;
 
-    system("cls");
+    SYSTEM_CLS;
 
     //打开账户文件
     file = fopen("member.txt", "r");
@@ -133,12 +140,11 @@ void registry(void)
 void show_menu(void)
 {
     int operate = 0;
-    struct port *ports = NULL;//端口信息
-    ports = malloc(sizeof(struct port));
+    node_t *head = list_init();//端口信息
     static int num = 0;
 
     while(1){
-        system("cls");
+        SYSTEM_CLS;
         printf("请输入要执行的操作：\n");
         printf("1. 查看端口\n");
         printf("2. 设置端口\n");
@@ -148,38 +154,42 @@ void show_menu(void)
         scanf("%d", &operate);
 
         switch(operate){
-            case 1: show_ports(ports, num);
+            case 1: show_ports(head);
                 break;
-            case 2: set_ports(&ports, &num);
+            case 2: set_ports(head, &num);
                 break;
             case 3:
-                system("cls");
+                SYSTEM_CLS;
+                destroy_list(&head);
+                num = 0;
                 return ;
             default: show_error("请求的操作不合法");
         }
     }
-
-    free(ports);
     
 }
 
-void show_ports(struct port *p, int num)
+void show_ports(node_t *head)
 {
-    system("cls");
-    for(int i=0; i<num; i++)
-    {
-        printf("名称：%s\t", p[i].name);
-        printf("状态：%s\t", p[i].status ? "激活" : "禁用");
-        printf("IP：%s\t", p[i].ip);
-        printf("类型：%s\t", p[i].type);
+    check_head(head);
+
+    SYSTEM_CLS;
+    node_t *p = head->next;
+    while(p){
+        printf("名称：%s\t", p->node->name);
+        printf("状态：%s\t", p->node->status ? "激活" : "禁用");
+        printf("IP：%s\t", p->node->ip);
+        printf("类型：%s\t", p->node->type);
         printf("\n");
+        p = p->next;
     }
+
     system("pause");
 }
 
-void set_ports(struct port **p, int *num)
+void set_ports(node_t *head, int *num)
 {
-    system("cls");
+    SYSTEM_CLS;
     char tmp;
 
     printf("请输入要查看的端口:\n");
@@ -189,6 +199,7 @@ void set_ports(struct port **p, int *num)
     }
 
     printf("+ 添加端口\n");
+    printf("- 删除端口\n");
     printf("q 退出\n");
 
     fflush(stdin);
@@ -201,15 +212,24 @@ void set_ports(struct port **p, int *num)
     if(tmp == '+')
     {
         int res;
-        res = add_port(*p, num);
+        res = add_port(head);
+        if(res == 1) *num += 1;//添加成功，总端口数+1
         return ;//添加成功或失败，返回上一级目录
+    }
+
+    if(tmp == '-')
+    {
+        int res;
+        res = del_port(head);
+        if(res == 1) *num -= 1;
+        return ;
     }
 
     int edit_num = tmp - '1';
 
     //暂时只允许添加9个端口(0-8)
     if(num >= 0 && edit_num < 9){
-        set_port(*p + edit_num);
+        set_port(head, edit_num);
     }else{
         printf("%d", num);
         show_error("没有这个端口");
@@ -224,33 +244,59 @@ void show_error(char *msg)
     system("pause");
 }
 
-void set_port(struct port *p)
+void set_port(node_t *head, int place)
 {
-    system("cls");
+    check_head(head);
+    SYSTEM_CLS;
+
+    //找到该节点
+    node_t *p = head->next;
+    int count = 0;
+    while(p){
+        if(count == place) break;
+        p = p->next;
+        count++;
+    }
+    if(p == NULL){
+        printf("找不到可以设置的端口\n");
+        exit(1);
+    }
+
     printf("请输入端口名：");
-    scanf("%s", p->name);
+    scanf("%s", p->node->name);
 
     printf("请输入端口状态：0：禁用，1：启用");
-    scanf("%d", &p->status);
+    scanf("%d", &p->node->status);
 
     printf("请输入ip地址：");
-    scanf("%s", p->ip);
+    scanf("%s", p->node->ip);
 
     printf("请输入端口类型（LAN/WAN）：");
-    scanf("%s", p->type);
+    scanf("%s", p->node->type);
 }
 
-int add_port(struct port *p, int *num)
+int add_port(node_t *head)
 {
-    int re_size = (*num + 1) * sizeof(struct port);
-    p = (struct port *)realloc(p, re_size);
-    if(p == NULL){
-        show_error("动态添加内存失败");
+    check_head(head);
+
+    //找到最后一个节点
+    node_t *p = head;
+    int num = 0;//计算当前节点的位置
+
+    while(p->next){
+        p = p->next;
+        num++;
     }
 
     //设置端口
-    set_port(p + *num);
-    *num += 1;
+    node_t *insert = (node_t *)malloc(sizeof(node_t));
+    insert->node = (elem_t *)malloc(sizeof(elem_t));
+    insert->next = NULL;
+    p->next = insert;
+
+    //让用户输入端口信息
+    set_port(head, num);
+
     return 1;
 }
 
@@ -261,5 +307,95 @@ node_t *list_init(void){
         return NULL;
     }
 
+    head->next = NULL;
+
     return head;
+}
+
+void check_head(node_t *head)
+{
+    if(head == NULL)
+    {
+        printf("传入的头文件为空\n");
+        exit(1);
+    }
+}
+
+int empty_list(node_t *head, free_fun_t fun)
+{
+    check_head(head);
+
+    node_t *p = head->next;
+    node_t *first = NULL;
+    int count = 0;
+
+    while(p){
+        first = p;
+        p = p->next;
+        fun(p);
+        count++;
+    }
+
+    return count;
+}
+
+int destroy_list(node_t **head)
+{
+    empty_list(*head, free_list_node);
+    free(*head);
+    *head = NULL;
+
+    return 1;
+}
+
+int del_port(node_t *head)
+{
+    check_head(head);
+
+    int place = 0;
+    fflush(stdin);
+    printf("请输入要删除的端口号:");
+    scanf("%c", &place);
+
+    place -= '0';
+    if(place <1 || place > 9){
+        printf("请输入1-9的端口\n");
+        return 0;
+    }
+
+    node_t *pre = head;
+    node_t *p = head->next;
+    int count = 0;
+
+    while(p){
+        count++;
+        if(count == place) break;
+        p = p->next;
+        pre = pre->next;
+    }
+
+    if(count != place){
+        printf("找不到要删除的端口号\n");
+        return 0;
+    }
+
+    pre->next = p->next;
+    free(p->node);
+    free(p);
+
+    return 1;
+}
+
+int free_list_node(node_t *node)
+{
+    if(node == NULL)
+    {
+        printf("传入的节点为NULL\n");
+        return 0;
+    }
+
+    free(node->node);
+    free(node);
+
+    return 1;
 }
