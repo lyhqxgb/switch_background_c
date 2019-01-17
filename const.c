4a21248e99c0b4e6ae0e80ba228216ff16824e43
lyhqxgb <lyh52xx@126.com>
2019-01-17 23:56:04 +0800

    learn const

---
+++ const.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //第一种指针常量，指针可以多次改变指向的地址，但不能改变指向的值
+    const int *p_int1;
+    int int1 = 10;
+    p_int1 = &int1;
+    printf("*p_int1 = %d\n", *p_int1);
+
+    //*p_int1 = 20; //这样是错误的，不能改变指向的值
+    int1 = 20; //改变原来变量的值不受影响，p_int1也可以取到改变后的值
+    printf("*p_int1 = %d\n", *p_int1);
+
+
+    //第二种指针常量，指针不能改变指向的地址，但可以改变指向的值
+    int int2 = 10;
+    int * const p_int2 = &int2;
+    printf("*p_int2 = %d\n", *p_int2);
+
+    //p_int2 = NULL; //编译错误，p_int2的值不能再改变了
+
+    *p_int2 = 20; //但是p_int2可以改变指向的值
+    printf("*p_int2 = %d\n", *p_int2);
+
+    return 0;
+}
