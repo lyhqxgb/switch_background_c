e2f77175f15c13244d7addbb4ede0e2026f6b97c
lyhqxgb <lyh52xx@126.com>
2019-01-21 23:24:34 +0800

    add new usage of const

--- const.c
+++ const.c
@@ -4,6 +4,7 @@ int main(void)
 {
     //第一种指针常量，指针可以多次改变指向的地址，但不能改变指向的值
     const int *p_int1;
+    //int const *p_int1; //这样写效果一样
     int int1 = 10;
     p_int1 = &int1;
     printf("*p_int1 = %d\n", *p_int1);
@@ -22,6 +23,13 @@ int main(void)
 
     *p_int2 = 20; //但是p_int2可以改变指向的值
     printf("*p_int2 = %d\n", *p_int2);
+
+    //第三种指针常量，指针不能改变指向的地址，也不能改变指向的值
+    const int * const p_int3 = &int2;
+    printf("*p_int3 = %d\n", *p_int3);
+    //其它变量改变指向的值，指针的值也会改变
+    int2 = 30;
+    printf("*p_int3 = %d\n", *p_int3);
 
     return 0;
 }
